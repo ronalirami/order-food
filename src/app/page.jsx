71e8a1147b4576f826ebbda377aa8d704bfc1e2a
Ronal Irami <r.aochi87@gmail.com>
2026-03-03 21:35:40 +0900

    editpage.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,44 @@
+import Image from "next/image";
+import Link from "next/link";
+import heroMain from "@/assets/images/hero-main.png";
+import heroSide1 from "@/assets/images/hero-side-1.jpg";
+import heroSide2 from "@/assets/images/hero-side-2.jpg";
+import heroSide3 from "@/assets/images/hero-side-3.jpg";
 export default function HomePage() {
   return (
-    <section className="relative w-full h-screen flex overflow-hidden">
-      {/* === BACKGROUND VIDEO === */}
+    <section className="w-full h-screen flex overflow-hidden ">
+      {/* Left Hero */}
       <div className="relative flex-1 rounded-2xl overflow-hidden mr-2">
         <Image
           src={heroMain}
-          alt="Menu utama"
+          alt="Hero"
           fill
-          priority
           className="object-cover brightness-50"
-        />  
+        />
+      </div>
+
+      {/* Right Grid */}
+      <div className="w-1/4 grid grid-rows-3 gap-2">
+        <HeroCard image={heroSide1} link="/menu" label="Menu" />
+        <HeroCard image={heroSide2} link="/order" label="Reservasi" />
+        <HeroCard image={heroSide3} link="/tentang" label="Tentang" />
       </div>
     </section>
   );
+}
+
+function HeroCard({ image }) {
+  return (
+    <div className="relative">
+      <Image
+        src={image}
+        alt="Hero"
+        fill
+        className="object-cover"
+      />
+
+      <div className="absolute inset-0 bg-gradient-to-t from-black/50 to-transparent"></div>
+
+    </div>
+  );
+}
